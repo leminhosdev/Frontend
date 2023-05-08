06eb2004fa206f0d2954497f35db7022889cac2b
leminhosdev <lucaslemos8294@gmail.com>
2023-05-08 19:18:40 -0300

    listando, mudando interface e consumindo o backend

--- src/app/components/views/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categoria-read',
   templateUrl: './categoria-read.component.html',
   styleUrls: ['./categoria-read.component.css']
 })
-export class CategoriaReadComponent {
 
+export class CategoriaReadComponent implements OnInit{
+  constructor() { } 
+  ngOnInit(): void {
+      
+      
+    }
+
+    displayedColumns: string[] = ['id', 'nome', 'descricao', 'acoes'];
 }
